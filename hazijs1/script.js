5f1e8cd3b78676c1d9f6719ee44386f92957f56c
Atomanti74 <89605546+Atomanti74@users.noreply.github.com>
2024-11-10 17:19:48 +0000

    java hozzaadva

---
+++ hazijs1/script.js
@@ -0,0 +1,30 @@
+console.log("Anti");
+
+
+let nameCity = "UK" ;
+console.log(nameCity);
+
+
+let  nameCountry= "Harrogate" ;
+console.log(nameCountry);
+
+
+console.log("Harrogate, UK");
+
+let x = 9
+console.log(x);
+
+console.log(x + 1)
+
+var  y = 20
+
+console.log(x+y)
+
+console.log(x*y)
+
+var z = 2641
+console.log(z/7)
+
+let favouriteÁllat = prompt( "Mi a kedvenc állatod?");
+
+console.log("A kedvenc állatod: " + favouriteÁllat );
